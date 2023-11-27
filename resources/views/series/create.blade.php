b4f3bcad00db43b54c0920130550ed5ce00d695b
Adauto Júnior <dev.adautojunior@gmail.com>
2023-11-27 17:49:14 -0300

    feat: class 04 of the 'Laravel: criando uma aplicação com MVC' course

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,6 +1,10 @@
 <x-layout title='Nova Série'>
   <form action='' method='post'>
-    <label for='name'>Nome: </label>
-    <input type='text' id='name' name='name'>
+    <div class='mb-3'>
+      <label class='form-label' for='name'>Nome: </label>
+      <input class='form-control' type='text' id='name' name='name'>
+    </div>
+
+    <button type='submit' class='btn btn-primary'>Adicionar</button>
   </form>
 </x-layout>
